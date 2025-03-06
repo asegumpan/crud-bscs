956036d1a492769b6edfdd0c1b4419cfa3ec1ad0
Lightskent <kenttnek26@gmail.com>
2025-03-06 13:24:55 +0800

    First commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,29 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PostController extends Controller
 {
-    //
+    /**
+     * Show the form to create a new blog post.
+     */
+    public function create(): View
+    {
+        return view('post.create');
+    }
+
+    /**
+     * Store a new blog post.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // Validate and store the blog post...
+
+        $post = /** ... */
+
+        return to_route('post.show', ['post' => $post->id]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/post/create', [PostController::class, 'create']);
+Route::post('/post', [PostController::class, 'store']);
