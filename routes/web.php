91aaa71e142702544afcabb6b1bd54182a4a84df
arnel78 <sayamarnel46@gmail.com>
2025-03-11 12:37:57 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/users', [UserController::class, 'index'])->name('user.page.index');
+Route::get('user/{id}', [UserController::class, 'show' ])->name('user.page.show');
