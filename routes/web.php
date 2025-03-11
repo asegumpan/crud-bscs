43f19ada578a105ddf38f656d5b85319e672524c
navi1000sage <c20222540.ybanez@csav.edu.ph>
2025-03-11 14:09:04 +0800

    User View (Blade template) delacruz

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Users;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
